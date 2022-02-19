0b5d5869c8225f887bf07340c01911f6e3556d11
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-02-20 00:55:33 +0700

    express route

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ const PORT = 5000;
 
 app.use(bodyParser.json());
 
-app.listen(PORT, () => console.log(`Server running on port://localhost:${PORT}`))
+app.get('/', (req, res) => {
+    console.log('[TEST]!');
+    
+    res.send('Hello from Homepage');
+});
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`))
