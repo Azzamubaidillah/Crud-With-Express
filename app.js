9017c81cf0a6dfe45408a471dafbfa3078041d6e
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-02-23 02:25:30 +0700

    delete user

--- app.js
+++ app.js
@@ -28,6 +28,23 @@ app.get('/users', (_,res) => {
     res.json(users);
 });
 
+app.delete('/delete', (req, res) => {
+    const { username, password } = req.body;
+
+    const existingUser = users.find(
+        u => u.username === username && u.password === password
+    );
+    console.log(existingUser);
+
+    if (!existingUser) {
+        res.statusCode(401).json({ errorStatus: "Credentials did not match" });
+    }
+
+    users.splice(users.indexOf(existingUser), 1);
+    res.json(users);
+
+})
+
 //connect to DB
 
 mongoose.connect(
