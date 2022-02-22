76011ba5f9a53286f714dc3d5537f2948283b961
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-02-23 01:28:03 +0700

    get user

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import 'dotenv/config';
-import './routes/posts.js';
+
+
 
 const app = express();
 
-const postsRoute = router;
-
-app.use('/posts', postsRoute);
-
-// Routes
 app.get('/', (req, res) => {
-    res.send('We are on home');
+    const user = req.query.user;
+
+    res.send(user + "!!");
 });
 
-app.get('/posts', (req, res) => {
-    res.send('We are on posts');
-});
 
 //connect to DB
 
